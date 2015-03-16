3e713931a56eb61896163350e46428e31ac5ed49
threewater <threewaterL@163.com>
2015-03-16 21:43:26 +0800

    combine wisehmi.c and wisehmi_spl.c

--- board/autorock/wisehmi/wisehmi.c
+++ board/autorock/wisehmi/wisehmi.c
@@ -1,4 +1,7 @@
 #include <common.h>
+#ifdef CONFIG_SPL_BUILD
+#include <spl.h>
+#endif
 #include <asm/io.h>
 #include <asm/arch/clock.h>
 #include <asm/arch/imx-regs.h>
@@ -33,42 +36,20 @@ DECLARE_GLOBAL_DATA_PTR;
 #define SPI_PAD_CTRL (PAD_CTL_HYS | PAD_CTL_SPEED_MED |		\
 	PAD_CTL_DSE_40ohm     | PAD_CTL_SRE_FAST)
 
-int dram_init(void)
-{
-	gd->ram_size = get_ram_size((void *)PHYS_SDRAM, PHYS_SDRAM_SIZE);
-
-	return 0;
-}
-
+#if defined(CONFIG_SPL_SERIAL_SUPPORT) || !defined(CONFIG_SPL_BUILD)
 static iomux_v3_cfg_t const uart4_pads[] = {
 	MX6_PAD_KEY_COL0__UART4_TX_DATA | MUX_PAD_CTRL(UART_PAD_CTRL),
 	MX6_PAD_KEY_ROW0__UART4_RX_DATA | MUX_PAD_CTRL(UART_PAD_CTRL),
 };
-
-static iomux_v3_cfg_t const enet_pads[] = {
-	// RMII
-	MX6_PAD_ENET_MDIO__ENET_MDIO      | MUX_PAD_CTRL(ENET_PAD_CTRL),
-	MX6_PAD_ENET_MDC__ENET_MDC        | MUX_PAD_CTRL(ENET_PAD_CTRL),
-	MX6_PAD_ENET_CRS_DV__ENET_RX_EN   | MUX_PAD_CTRL(ENET_PAD_CTRL),
-	MX6_PAD_ENET_RX_ER__ENET_RX_ER    | MUX_PAD_CTRL(ENET_PAD_CTRL),
-	MX6_PAD_ENET_TX_EN__ENET_TX_EN    | MUX_PAD_CTRL(ENET_PAD_CTRL),
-	MX6_PAD_ENET_RXD0__ENET_RX_DATA0  | MUX_PAD_CTRL(ENET_PAD_CTRL),
-	MX6_PAD_ENET_RXD1__ENET_RX_DATA1  | MUX_PAD_CTRL(ENET_PAD_CTRL),
-	MX6_PAD_ENET_TXD0__ENET_TX_DATA0  | MUX_PAD_CTRL(ENET_PAD_CTRL),
-	MX6_PAD_ENET_TXD1__ENET_TX_DATA1  | MUX_PAD_CTRL(ENET_PAD_CTRL),
-	// reset
-	MX6_PAD_EIM_EB3__GPIO2_IO31       | MUX_PAD_CTRL(NO_PAD_CTRL),
-	// interrupt
-	MX6_PAD_ENET_REF_CLK__GPIO1_IO23  | MUX_PAD_CTRL(NO_PAD_CTRL),
-	// reference clock
-	MX6_PAD_GPIO_16__ENET_REF_CLK     | MUX_PAD_CTRL(NO_PAD_CTRL),
-};
-
-static void setup_iomux_enet(void)
+static void setup_iomux_uart(void)
 {
-	imx_iomux_v3_setup_multiple_pads(enet_pads, ARRAY_SIZE(enet_pads));
+	imx_iomux_v3_setup_multiple_pads(uart4_pads, ARRAY_SIZE(uart4_pads));
 }
+#else
+#define setup_iomux_uart()
+#endif
 
+#if (!defined(CONFIG_SPL_BUILD)&&defined(CONFIG_FSL_ESDHC)) || (defined(CONFIG_SPL_BUILD)&&defined(CONFIG_SPL_MMC_SUPPORT))
 static iomux_v3_cfg_t const usdhc2_pads[] = {
 	MX6_PAD_SD2_CLK__SD2_CLK   | MUX_PAD_CTRL(USDHC_PAD_CTRL),
 	MX6_PAD_SD2_CMD__SD2_CMD   | MUX_PAD_CTRL(USDHC_PAD_CTRL),
@@ -91,12 +72,6 @@ static iomux_v3_cfg_t const usdhc3_pads[] = {
 	MX6_PAD_SD3_DAT7__SD3_DATA7 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
 };
 
-static void setup_iomux_uart(void)
-{
-	imx_iomux_v3_setup_multiple_pads(uart4_pads, ARRAY_SIZE(uart4_pads));
-}
-
-#ifdef CONFIG_FSL_ESDHC
 static struct fsl_esdhc_cfg usdhc_cfg[2] = {
 	{USDHC2_BASE_ADDR},
 	{USDHC3_BASE_ADDR},
@@ -108,6 +83,28 @@ int board_mmc_getcd(struct mmc *mmc)
 	return 1;
 }
 
+#ifdef CONFIG_SPL_BUILD
+int board_mmc_init(bd_t *bis)
+{
+	int device = spl_boot_device();
+	switch (device) {
+	case BOOT_DEVICE_MMC1:
+		imx_iomux_v3_setup_multiple_pads(usdhc2_pads, ARRAY_SIZE(usdhc2_pads));
+		enable_usdhc_clk(1, 2);
+		usdhc_cfg[0].sdhc_clk = mxc_get_clock(MXC_ESDHC2_CLK);
+		usdhc_cfg[0].max_bus_width = 4;
+		return fsl_esdhc_initialize(bis, &usdhc_cfg[0]);
+	case BOOT_DEVICE_MMC2:
+		imx_iomux_v3_setup_multiple_pads(usdhc3_pads, ARRAY_SIZE(usdhc3_pads));
+		enable_usdhc_clk(1, 3);
+		usdhc_cfg[1].sdhc_clk = mxc_get_clock(MXC_ESDHC3_CLK);
+		usdhc_cfg[1].max_bus_width = 8;
+		return fsl_esdhc_initialize(bis, &usdhc_cfg[1]);
+	default:
+		return -1;
+	}
+}
+#else
 int board_mmc_init(bd_t *bis)
 {
 	int status = 0;
@@ -126,9 +123,10 @@ int board_mmc_init(bd_t *bis)
 
 	return status;
 }
-#endif
+#endif	//#ifdef CONFIG_SPL_BUILD
+#endif	//#if defined(CONFIG_FSL_ESDHC) || (defined(CONFIG_SPL_BUILD)&&defined(CONFIG_SPL_MMC_SUPPORT))
 
-#ifdef CONFIG_MXC_SPI
+#if (!defined(CONFIG_SPL_BUILD)&&defined(CONFIG_MXC_SPI)) || (defined(CONFIG_SPL_BUILD)&&defined(CONFIG_BOOT_SPI))
 static iomux_v3_cfg_t const ecspi2_pads[] = {
 	/* SS2 */
 	MX6_PAD_CSI0_DAT11__GPIO5_IO29 | MUX_PAD_CTRL(NO_PAD_CTRL),
@@ -147,9 +145,9 @@ int board_spi_cs_gpio(unsigned bus, unsigned cs)
 {
 	return (bus == 1 && cs == 0) ? (IMX_GPIO_NR(5, 29)) : -1;
 }
-#endif
+#endif	//#if defined(CONFIG_MXC_SPI) || (defined(CONFIG_SPL_BUILD)&&defined(CONFIG_BOOT_SPI))
 
-#ifdef CONFIG_NAND_MXS
+#if (!defined(CONFIG_SPL_BUILD)&&defined(CONFIG_NAND_MXS)) || (defined(CONFIG_SPL_BUILD)&&defined(CONFIG_SPL_NAND_SUPPORT))
 iomux_v3_cfg_t nfc_pads[] = {
 	MX6_PAD_NANDF_CLE__NAND_CLE		| MUX_PAD_CTRL(NO_PAD_CTRL),
 	MX6_PAD_NANDF_ALE__NAND_ALE		| MUX_PAD_CTRL(NO_PAD_CTRL),
@@ -191,7 +189,89 @@ static void setup_gpmi_nand(void)
 	/* enable apbh clock gating */
 	setbits_le32(&mxc_ccm->CCGR0, MXC_CCM_CCGR0_APBHDMA_MASK);
 }
+#endif	//#if defined(CONFIG_NAND_MXS) || (defined(CONFIG_SPL_BUILD)&&defined(CONFIG_SPL_NAND_SUPPORT))
+
+int board_early_init_f(void)
+{
+	setup_iomux_uart();
+	return 0;
+}
+
+#ifdef CONFIG_SPL_BUILD
+void board_init_f(ulong dummy)
+{
+	arch_cpu_init();
+
+	/* Clear the BSS. */
+	memset(__bss_start, 0, __bss_end - __bss_start);
+
+	/* Set global data pointer. */
+	gd = &gdata;
+
+	board_early_init_f();
+
+	board_init_r(NULL, 0);
+}
+
+void spl_board_init(void)
+{
+	get_clocks();
+#ifdef CONFIG_SPL_SERIAL_SUPPORT
+	preloader_console_init();
 #endif
+
+#ifdef CONFIG_SPL_TLB_ADDRESS
+	gd->arch.tlb_addr = CONFIG_SPL_TLB_ADDRESS;
+	gd->arch.tlb_size = PGTABLE_SIZE;
+	gd->bd->bi_dram[0].start = CONFIG_SYS_SDRAM_BASE;
+	gd->bd->bi_dram[0].size = PHYS_SDRAM_SIZE;
+	enable_caches();
+#endif
+#ifdef CONFIG_BOOT_SPI
+	setup_spi();
+#endif
+
+#ifdef CONFIG_SPL_NAND_SUPPORT
+	setup_gpmi_nand();
+#endif
+}
+
+int spl_start_uboot(void)
+{
+	// start uboot when console has input
+	mdelay(1);
+	return tstc();
+}
+
+void reset_cpu(ulong addr)
+{
+
+}
+
+#else
+static iomux_v3_cfg_t const enet_pads[] = {
+	// RMII
+	MX6_PAD_ENET_MDIO__ENET_MDIO      | MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET_MDC__ENET_MDC        | MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET_CRS_DV__ENET_RX_EN   | MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET_RX_ER__ENET_RX_ER    | MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET_TX_EN__ENET_TX_EN    | MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET_RXD0__ENET_RX_DATA0  | MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET_RXD1__ENET_RX_DATA1  | MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET_TXD0__ENET_TX_DATA0  | MUX_PAD_CTRL(ENET_PAD_CTRL),
+	MX6_PAD_ENET_TXD1__ENET_TX_DATA1  | MUX_PAD_CTRL(ENET_PAD_CTRL),
+	// reset
+	MX6_PAD_EIM_EB3__GPIO2_IO31       | MUX_PAD_CTRL(NO_PAD_CTRL),
+	// interrupt
+	MX6_PAD_ENET_REF_CLK__GPIO1_IO23  | MUX_PAD_CTRL(NO_PAD_CTRL),
+	// reference clock
+	MX6_PAD_GPIO_16__ENET_REF_CLK     | MUX_PAD_CTRL(NO_PAD_CTRL),
+};
+
+static void setup_iomux_enet(void)
+{
+	imx_iomux_v3_setup_multiple_pads(enet_pads, ARRAY_SIZE(enet_pads));
+}
 
 int board_eth_init(bd_t *bis)
 {
@@ -222,13 +302,6 @@ int board_eth_init(bd_t *bis)
 
 u32 get_board_rev(void)
 {
-	return 0;
-}
-
-int board_early_init_f(void)
-{
-	setup_iomux_uart();
-
 	return 0;
 }
 
@@ -269,3 +342,12 @@ int checkboard(void)
 {
 	return 0;
 }
+
+int dram_init(void)
+{
+	gd->ram_size = get_ram_size((void *)PHYS_SDRAM, PHYS_SDRAM_SIZE);
+
+	return 0;
+}
+
+#endif //#if defined(CONFIG_NAND_MXS) || (defined(CONFIG_SPL_BUILD)&&defined(CONFIG_SPL_NAND_SUPPORT))
